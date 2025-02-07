1968737c63d725aee4329edb710e2940b6e20c78
priyankaar02 <priyankaar02@gmail.com>
2025-02-07 11:00:28 +0100

    Vite & React Currency Converter App (Learn about creating custom hooks and components)

--- 06currencyConverter/src/App.jsx
+++ 06currencyConverter/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import { useState } from "react";
 import useCurrencyInfo from "./hooks/useCurrencyInfo";
 import { InputBox } from "./components/index.js";
 import "./App.css";
